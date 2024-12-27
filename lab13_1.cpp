c523d34250100ed137b0739ab8f2466943947ae3
Jorge <57448609+jorgejord@users.noreply.github.com>
2024-12-27 18:54:55 +0700

    Add files via upload

---
+++ lab13_1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+void stat(const double[],int,double[]);
+
+int main(){
+    double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
+    int N = sizeof(A)/sizeof(A[0]);
+    double B[6];
+    stat(A,N,B);
+    cout << fixed << setprecision(2);
+    cout << "Arithmetic Mean = " << B[0] << endl;
+    cout << "Standard Deviation = " << B[1] << endl;
+    cout << "Geometric Mean = " << B[2] << endl;
+    cout << "Harmonic Mean = " << B[3] << endl;
+    cout << "Max = " << B[4] << endl;
+    cout << "Min = " << B[5];
+    return 0;
+}
+
+void stat(const double num[],int n,double output[]){
+    double sum = 0,product = 1.0 , sumPow2 = 0,Hsum = 0,max = num[0] ,min = num[0];
+    for(int i = 0;i < n; i++) {    
+        sum += num[i];
+        sumPow2 += num[i]*num[i];
+        product *= num[i];
+        Hsum += 1/num[i];
+        if(num[i] > max ) max = num[i];
+        else if(num[i] < min ) min = num[i];
+    }
+    output[0] = sum/n; 
+    output[1] = sqrt((sumPow2/n)-output[0]*output[0]);
+    output[2] = pow(product,(1.0/n));
+    output[3] = n/(Hsum);
+    output[4] = max;
+    output[5] = min;
+}
---
+++ lab13_2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const int N = 30;
+const int M = 70;
+
+void updateImage(bool [][M],int,int,int);
+
+void showImage(const bool [][M]);
+
+int main()
+{
+    bool image[N][M] = {};
+    int s,x,y;
+    do{
+        showImage(image);
+        cout << "Input your brush size and location: ";
+        cin >> s >> x >> y;
+        updateImage(image,s,x,y);
+    }while(s != 0 || x != 0 || y != 0);
+    return 0;
+}
+
+void showImage(const bool size[][M]){
+    cout << "------------------------------------------------------------------------\n";
+    for(int i = 0; i< N ;i++ ){
+        cout << "|";  
+        for(int j = 0 ; j < M; j++){
+            if( size[i][j] ) cout << "*";
+            else cout << " ";
+        }
+        cout << "|";
+        cout << endl;
+        }
+    cout << "------------------------------------------------------------------------\n";
+    }
+
+void updateImage(bool img[][M] ,int s,int x,int y){
+    if(s == 1){
+        img[x][y] = 1;
+    }
+    else {
+        for(int i = 0;i < N ;i++){
+            for(int j = 0 ; j < M;j++){
+                if (sqrt(pow((i-x),2)+pow((j-y),2)) <= s-1) img[i][j] = 1;
+            }
+        }
+    }
+}
